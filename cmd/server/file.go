45b3fb276bdc411078625e44658bb92a6c006dd4
快醒醒你只是条单身汪 <langbiantianya@outlook.com>
2023-12-15 15:51:44 +0800

    feat: 添加第一个方法

--- cmd/server/file.go
+++ cmd/server/file.go
@@ -1,13 +1,32 @@
 package server
 
-import "io/fs"
+import (
+	"io/fs"
+	"log"
+	"os"
+)
 
-type Basic struct {
-	RootPath string
+type Basic interface {
+	Ls() SelectedPath
 }
 
 type SelectedPath struct {
-	Basic   Basic
-	Parent  string
-	Entries []fs.DirEntry
+	RootPath string
+	Parent   string
+	NowPath  string
+	Entries  []fs.DirEntry
+}
+
+func (b *SelectedPath) Ls() *SelectedPath {
+	dir, err := os.Open(b.RootPath)
+	if err != nil {
+		log.Default().Println(err)
+	}
+	defer dir.Close()
+	entries, err := dir.ReadDir(0)
+	if err != nil {
+		println(err)
+	}
+	b.Entries = entries
+	return b
 }
--- main.go
+++ main.go
@@ -1,29 +1,21 @@
 package main
 
-import "os"
+import "simpleFileServer/cmd/server"
 
 func main() {
-	rootPath := "./"
-	dir, err := os.Open(rootPath)
-	if err != nil {
-		println(err)
+	root := server.SelectedPath{
+		RootPath: "./",
+		Parent:   "./",
+		NowPath:  "./",
+		Entries:  nil,
 	}
-	defer dir.Close()
-	entries, err := dir.ReadDir(0)
-	if err != nil {
-		println(err)
-	}
-	for _, entry := range entries {
+	res := root.Ls()
+	for _, entry := range res.Entries {
 		if entry.IsDir() {
 			println("目录:", entry.Name())
 		} else {
 			println("文件:", entry.Name())
 		}
 	}
-	// r := cmd.SetupRouter()
-	// // Listen and Server in 0.0.0.0:8080
-	// err := r.Run(":8080")
-	// if err != nil {
-	// 	return
-	// }
+
 }
