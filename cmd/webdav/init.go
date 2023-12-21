77b1854e526882d59e89906f90f9c36aa9eca365
快醒醒你只是条单身汪 <langbiantianya@outlook.com>
2023-12-21 15:28:23 +0800

    feat: 完善异常日志输出

--- cmd/webdav/init.go
+++ cmd/webdav/init.go
@@ -12,6 +12,8 @@ func InitWebDav(r *gin.Engine, b *server.SelectedPath) {
 	r.Use(serve("/webdav", b.RootPath, func(c *gin.Context) bool {
 		return true
 	}, func(req *http.Request, err error) {
-		log.Default().Println(req.URL.Path, err)
+		if err != nil {
+			log.Default().Println(req.URL.Path, err)
+		}
 	}))
 }
