991d8b174eca7882d1eebac79c5f840528da93bf
快醒醒你只是条单身汪 <langbiantianya@outlook.com>
2024-01-04 10:35:08 +0800

    feat: 添加todo

--- cmd/plugins/webdav/init.go
+++ cmd/plugins/webdav/init.go
@@ -14,6 +14,7 @@ import (
 func InitWebDav(r *gin.Engine, ctx *common.ServerContext) {
 	r.Use(serve("/webdav", ctx.WorkHome, func(c *gin.Context) bool {
 		username, _, _ := plugins.ParseAuthHeader(c.Request.Header.Get("Authorization"))
+		// TODO 👇要重构
 		if ctx.MultipleUser {
 			return plugins.BasicAuth(c, ctx) && account.VerifyRights(ctx.Acctx, username, func(operations rights.FileOperations) bool {
 				return rights.Verify(c, operations)
