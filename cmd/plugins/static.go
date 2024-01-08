26a2b1c85ef843e760358ff416cb8086579c5b56
快醒醒你只是条单身汪 <langbiantianya@outlook.com>
2024-01-08 10:36:51 +0800

    fix: 修改静态资源代理方式

--- cmd/plugins/static.go
+++ cmd/plugins/static.go
@@ -8,7 +8,9 @@ import (
 
 func InitWebStatic(r *gin.Engine) *gin.Engine {
 	r.GET("/", func(ctx *gin.Context) {
-		ctx.Redirect(http.StatusMovedPermanently, "/static")
+		ctx.Header("Content-Type", "text/html")
+		ctx.File("./static/index.html")
+		// ctx.Redirect(http.StatusMovedPermanently, "/static")
 	})
 	r.Static("/static", "./static")
 	r.StaticFS("/assets", http.Dir("./static/assets"))
